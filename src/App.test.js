78474207808eff9ea2755d936e9ff038073317ad
didapatria <didapatria3@gmail.com>
2022-06-10 23:12:13 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react'
+/* eslint-disable jest/valid-title */
+import { render, cleanup, screen } from '@testing-library/react'
 import App from './App'
+import '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+import { Provider } from 'react-redux'
+import store from './store'
+
+beforeEach(() => {
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+});
+
+afterEach(cleanup);
+
+// Definition of test
+describe('UI Test', () => {
+  test('renders button register on navbar', () => {
+    const register = screen.getByText(/Register/i)
+    expect(register).toBeInTheDocument()
+  });
+
+  // test('renders image service', () => {
+  //   const img_service = screen.getByRole('img')
+  //   expect(img_service).toHaveAttribute('src', './img/img_service.png')
+  //   expect(img_service).toHaveAttribute('alt', 'service')
+  // });
 })
+
+describe('Functional Test', () => {
+})
+
